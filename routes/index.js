b9706a761f09a9d98d6c8aa08a5acaaec83088d9
tombetthauser <tombetthauser@gmail.com>
2021-03-09 19:24:28 -0800

    Style login page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Actual Art Forum' });
+  res.render('index');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('GET /users page!');
+});
+
+router.get('/login', function(req, res, next) {
+  // res.send('GET /users/login page!');
+  res.render('users-login')
+});
+
+router.get('/register', function(req, res, next) {
+  res.send('GET /users/register page!');
 });
 
 module.exports = router;
