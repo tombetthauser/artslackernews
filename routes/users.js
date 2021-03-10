760142be6f99e637f879be280e72426d2ff4ddfb
tombetthauser <tombetthauser@gmail.com>
2021-03-10 12:08:13 -0800

    Add registration pug page

--- routes/users.js
+++ routes/users.js
@@ -8,62 +8,47 @@ const { loginUser, logoutUser } = require('../auth');
 
 const router = express.Router();
 
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(_req, res) {
   res.send('GET /users page!');
 });
 
-router.get('/login', csrfProtection, function(req, res, next) {
-  // res.send('GET /users/login page!');
+router.get('/login', csrfProtection, function(req, res) {
+  res.render('users-login', { csrfToken: req.csrfToken() })
+});
+
+router.get('/register', csrfProtection, function (req, res) {
   res.render('users-login', { csrfToken: req.csrfToken() })
 });
 
 const loginValidators = [
-  check('username')
-    .exists({ checkFalsy: true })
-    .withMessage('Please provide a username'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .withMessage('Please provide a password'),
+  check('username').exists({ checkFalsy: true }).withMessage('please provide a username'),
+  check('password').exists({ checkFalsy: true }).withMessage('please provide a password')
 ];
 
 router.post('/login', csrfProtection, loginValidators,
   asyncHandler(async (req, res) => {
-    const {
-      username,
-      password,
-    } = req.body;
-
+    const { username, password } = req.body;
     let errors = [];
     const validatorErrors = validationResult(req);
 
     if (validatorErrors.isEmpty()) {
       const user = await db.User.findOne({ where: { username } });
-
       if (user !== null) {
         const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
         if (passwordMatch) {
           loginUser(req, res, user);
-
-          // wrap redirect in session.save to force session update before redirecting
           req.session.save(() => {
             res.redirect(`/`);
           })
           return
         }
       }
-      
       errors.push('login failed for the provided username and password');
     } else {
       errors = validatorErrors.array().map((error) => error.msg);
     }
 
-    res.render('users-login', {
-      username,
-      errors,
-      csrfToken: req.csrfToken(),
-    });
+    res.render('users-login', { username, errors, csrfToken: req.csrfToken() });
   }));
 
 router.post('/logout', (req, res) => {
@@ -71,10 +56,6 @@ router.post('/logout', (req, res) => {
   req.session.save(() => {
     res.redirect('/');
   })
-});
-
-router.get('/register', function(req, res, next) {
-  res.send('GET /users/register page!');
 });
 
 module.exports = router;
