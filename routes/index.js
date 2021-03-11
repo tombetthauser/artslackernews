a0919f300400e07a647dbc1f9fa2f100a843343a
tombetthauser <tombetthauser@gmail.com>
2021-03-10 20:59:32 -0800

    Get fetch posts working in route and displaying in index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+router.get('/', asyncHandler(async (req, res) => {
+  const posts = await db.Post.findAll({ include: ["user"] });
+  res.render('index', { posts });
+}));
 
 module.exports = router;
