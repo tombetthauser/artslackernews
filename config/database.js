515800aa93373001772207897c526ccef61ba97c
tombetthauser <tombetthauser@gmail.com>
2021-03-25 23:09:08 -0700

    Update config database file

--- config/database.js
+++ config/database.js
@@ -1,6 +1,25 @@
-const {
-  db: { username, password, database, host },
-} = require('./index');
+// const {
+//   db: { username, password, database, host },
+// } = require('./index');
+
+// module.exports = {
+//   development: {
+//     username,
+//     password,
+//     database,
+//     host,
+//     dialect: 'postgres',
+//     seederStorage: 'sequelize',
+//   },
+// };
+
+const config = require("./index");
+
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
 
 module.exports = {
   development: {
@@ -8,7 +27,12 @@ module.exports = {
     password,
     database,
     host,
+    dialect: "postgres",
+    seederStorage: "sequelize"
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
-  },
+  }
 };
